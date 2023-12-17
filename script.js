e146e81dcbce934aa2966a39594eb8aa59d0f120
jtchap27 <josh.chap2@gmail.com>
2023-12-16 22:56:58 -0500

    Complete functioning calculator
    Every button on calculator works as intended. Still need to add functionality for a +/- button, a backspace button, and keyboard input support.

--- script.js
+++ script.js
@@ -1,48 +1,38 @@
-const add = function(...args) {
-  let sum = 0;
-  for (let i = 0; i < arguments.length; i++) {
-    sum += arguments[i];
-  }
-  return sum;
+const add = function(a, b) {
+  let answer = a + b;
+  return +(answer.toFixed(7));
 }
 
-const subtract = function(...args) {
-  let sum = arguments[0];
-  for (let i = 1; i < arguments.length; i++) {
-    sum -= arguments[i];
-  }
-  return sum;
+const subtract = function(a, b) {
+  let answer = a - b;
+  return +(answer.toFixed(7));
 }
 
-const multiply = function(...args) {
-  let sum = 1;
-  for (let i = 0; i < arguments.length; i++) {
-    sum *= arguments[i];
-  }
-  return sum;
+const multiply = function(a, b) {
+  let answer = a * b;
+  return +(answer.toFixed(7));
 }
 
-const divide = function(...args) {
-  let sum = arguments[0];
-  for (let i = 1; i < arguments.length; i++) {
-    sum /= arguments[i];
-  }
-  return sum;
+const divide = function(a, b) {
+  let answer = a / b;
+  return +(answer.toFixed(7));
 }
 
-let num1 = "number 1";
-let operator = "operator";
-let num2 = "number 2";
+const calc = {
+  firstNum: "",
+  op: "",
+  secondNum: "",
+};
 
-let operate = function([a, operator, b]) {
+let operate = function(a, operator, b) {
   if (operator === "+") {
-    return add(a, b);
+    return add(+calc.firstNum, +calc.secondNum);
   } else if (operator === "-") {
-    return subtract(a, b);
+    return subtract(+calc.firstNum, +calc.secondNum);
   } else if (operator === "x") {
-    return multiply(a, b);
+    return multiply(+calc.firstNum, +calc.secondNum);
   } else if (operator === "/") {
-    return divide(a, b);
+    return divide(+calc.firstNum, +calc.secondNum);
   }
 }
 
@@ -53,126 +43,140 @@ const displayValue = document.querySelector("#displayValue");
 
 const buttonZero = document.querySelector("#zero");
 buttonZero.addEventListener("click", () => {
-  displayInputArray.push(0);
-  displayInput.textContent = displayInputArray;
+  if (calc.firstNum !== "" && calc.op === "") {
+    calc.firstNum += 0;
+  } else if (calc.op !== "" && calc.secondNum !== "") {
+    calc.secondNum += 0;
+  };
+  displayInput.textContent = calc.firstNum + " " + calc.op + " " + calc.secondNum;
 });
 
 const buttonOne = document.querySelector("#one");
 buttonOne.addEventListener("click", () => {
-  displayInputArray.push(1);
-  displayInput.textContent = displayInputArray;
+  calc.op === "" ? calc.firstNum += 1 : calc.secondNum += 1
+  displayInput.textContent = calc.firstNum + " " + calc.op + " " + calc.secondNum;
 });
 
 const buttonTwo = document.querySelector("#two");
 buttonTwo.addEventListener("click", () => {
-  displayInputArray.push(2);
-  displayInput.textContent = displayInputArray;
+  calc.op === "" ? calc.firstNum += 2 : calc.secondNum += 2
+  displayInput.textContent = calc.firstNum + " " + calc.op + " " + calc.secondNum;
 });
 
 const buttonThree = document.querySelector("#three");
 buttonThree.addEventListener("click", () => {
-  displayInputArray.push(3);
-  displayInput.textContent = displayInputArray;
+  calc.op === "" ? calc.firstNum += 3 : calc.secondNum += 3;
+  displayInput.textContent = calc.firstNum + " " + calc.op + " " + calc.secondNum;
 });
 
 const buttonFour = document.querySelector("#four");
 buttonFour.addEventListener("click", () => {
-  displayInputArray.push(4);
-  displayInput.textContent = displayInputArray;
+  calc.op === "" ? calc.firstNum += 4 : calc.secondNum += 4
+  displayInput.textContent = calc.firstNum + " " + calc.op + " " + calc.secondNum;
 });
 
 const buttonFive = document.querySelector("#five");
 buttonFive.addEventListener("click", () => {
-  displayInputArray.push(5);
-  displayInput.textContent = displayInputArray;
+  calc.op === "" ? calc.firstNum += 5 : calc.secondNum += 5
+  displayInput.textContent = calc.firstNum + " " + calc.op + " " + calc.secondNum;
 });
 
 const buttonSix = document.querySelector("#six");
 buttonSix.addEventListener("click", () => {
-  displayInputArray.push(6);
-  displayInput.textContent = displayInputArray;
+  calc.op === "" ? calc.firstNum += 6 : calc.secondNum += 6
+  displayInput.textContent = calc.firstNum + " " + calc.op + " " + calc.secondNum;
 });
 
 const buttonSeven = document.querySelector("#seven");
 buttonSeven.addEventListener("click", () => {
-  displayInputArray.push(7);
-  displayInput.textContent = displayInputArray;
+  calc.op === "" ? calc.firstNum += 7 : calc.secondNum += 7
+  displayInput.textContent = calc.firstNum + " " + calc.op + " " + calc.secondNum;
 });
 
 const buttonEight = document.querySelector("#eight");
 buttonEight.addEventListener("click", () => {
-  displayInputArray.push(8);
-  displayInput.textContent = displayInputArray;
+  calc.op === "" ? calc.firstNum += 8 : calc.secondNum += 8
+  displayInput.textContent = calc.firstNum + " " + calc.op + " " + calc.secondNum;
 });
 
 const buttonNine = document.querySelector("#nine");
 buttonNine.addEventListener("click", () => {
-  displayInputArray.push(9);
-  displayInput.textContent = displayInputArray;
+  calc.op === "" ? calc.firstNum += 9 : calc.secondNum += 9
+  displayInput.textContent = calc.firstNum + " " + calc.op + " " + calc.secondNum;
 });
 
 const buttonDecimal = document.querySelector("#decimal");
 buttonDecimal.addEventListener("click", () => {
-  displayInputArray.push(".");
-  displayInput.textContent = displayInputArray;
+  if (calc.firstNum === "" && !calc.firstNum.includes(".")) {
+    calc.firstNum += "0.";
+  } else if (calc.op === "" && !calc.firstNum.includes(".")) {
+    calc.firstNum += ".";
+  };
+  if (calc.op !== "" && calc.secondNum === "" && !calc.secondNum.includes(".")) {
+    calc.secondNum += "0.";
+  } else if (calc.op !== "" && calc.secondNum !== "" && !calc.secondNum.includes(".")) {
+    calc.secondNum += ".";
+  };
+  displayInput.textContent = calc.firstNum + " " + calc.op + " " + calc.secondNum;
 });
 
 const buttonPlus = document.querySelector("#plus");
 buttonPlus.addEventListener("click", () => {
-  if (displayInputArray.length === 3) {
-    displayInputArray[0] = operate(displayInputArray);
-    displayValueArray[0] = operate(displayInputArray); 
-    displayValue.textContent =  displayValueArray[0];
+  if (calc.secondNum !== "") {
+    displayValue.textContent = operate(calc.firstNum, calc.op, calc.secondNum);
+    calc.firstNum = operate(calc.firstNum, calc.op, calc.secondNum);
+    calc.secondNum = "";
   }
-  if (displayValueArray.length > 0) {
-    displayInputArray = [];
-    displayInputArray[0] = displayValueArray[0];
-  };
-  displayInputArray.push("+");
-  displayInput.textContent = displayInputArray;
+  calc.op = "+";
+  displayInput.textContent = calc.firstNum + " " + calc.op;
 });
 
 const buttonSubtract = document.querySelector("#subtract");
 buttonSubtract.addEventListener("click", () => {
-  if (displayValueArray.length > 0) {
-    displayInputArray = [];
-    displayInputArray[0] = displayValueArray[0];
-  };
-  displayInputArray.push("-");
-  displayInput.textContent = displayInputArray;
+  if (calc.secondNum !== "") {
+    displayValue.textContent = operate(calc.firstNum, calc.op, calc.secondNum);
+    calc.firstNum = operate(calc.firstNum, calc.op, calc.secondNum);
+    calc.secondNum = "";
+  }
+  calc.op = "-";
+  displayInput.textContent = calc.firstNum + " " + calc.op;
 });
 
 const buttonMultiply = document.querySelector("#multiply");
 buttonMultiply.addEventListener("click", () => {
-  if (displayValueArray.length > 0) {
-    displayInputArray = [];
-    displayInputArray[0] = displayValueArray[0];
-  };
-  displayInputArray.push("x");
-  displayInput.textContent = displayInputArray;
+  if (calc.secondNum !== "") {
+    displayValue.textContent = operate(calc.firstNum, calc.op, calc.secondNum);
+    calc.firstNum = operate(calc.firstNum, calc.op, calc.secondNum);
+    calc.secondNum = "";
+  }
+  calc.op = "x";
+  displayInput.textContent = calc.firstNum + " " + calc.op;
 });
 
 const buttonDivide = document.querySelector("#divide");
 buttonDivide.addEventListener("click", () => {
-  if (displayValueArray.length > 0) {
-    displayInputArray = [];
-    displayInputArray[0] = displayValueArray[0];
-  };
-  displayInputArray.push("/");
-  displayInput.textContent = displayInputArray;
+  if (calc.secondNum !== "") {
+    displayValue.textContent = operate(calc.firstNum, calc.op, calc.secondNum);
+    calc.firstNum = operate(calc.firstNum, calc.op, calc.secondNum);
+    calc.secondNum = "";
+  }
+  calc.op = "/";
+  displayInput.textContent = calc.firstNum + " " + calc.op;
 });
 
 const buttonClear = document.querySelector("#clear");
 buttonClear.addEventListener("click", () => {
-  displayInputArray = [];
-  displayValueArray = [];
-  displayInput.textContent = displayInputArray;
-  displayValue.textContent = displayValueArray;
+  calc.firstNum = "";
+  calc.op = "";
+  calc.secondNum = "";
+  displayInput.textContent = "";
+  displayValue.textContent = "";
 });
 
 const buttonEnter = document.querySelector("#enter");
 buttonEnter.addEventListener("click", () => {
-  displayValueArray[0] = operate(displayInputArray); 
-  displayValue.textContent =  displayValueArray[0];
+  if (calc.secondNum !== "") {
+    displayValue.textContent = operate(calc.firstNum, calc.op, calc.secondNum)
+  };
 });
 
